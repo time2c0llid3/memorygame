ba0eb3de94910a62dc5eb1d4aeac9219bbbad5c0
Josh <jacksonsmacksonhub@pm.me>
2026-08-09 14:49:48 -0700

    Setup opengl to render a rectangle

--- include/game.h
+++ include/game.h
@@ -1,5 +1,10 @@
 #include <SDL3/SDL.h>
 #include <iostream>
+#include <glad/glad.h>
+#include <array>
+
+#define VERTSIZE 12
+#define INDEXSIZE 6
 
 class Game {
 	private:
@@ -8,8 +13,36 @@ class Game {
 		SDL_Window* m_window{ nullptr };
 		SDL_Renderer* m_renderer{ nullptr };
 		SDL_GLContext m_gContext{ nullptr };
+		/*------opengl rendering members-------*/
+		const char *vertexShaderSource = "#version 330 core\n"
+			"layout (location = 0) in vec3 aPos;\n"
+			"void main()\n"
+			"{\n"
+			"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+			"}\0";
+		const char *fragmentShaderSource = "#version 330 core\n"
+			"out vec4 FragColor;\n"
+			"void main()\n"
+			"{\n"
+			"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+			"}\n\0";
+			
+		const GLfloat vertices[VERTSIZE] = { 
+			-0.5f,  0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f, -0.5f, 0.0f,
+		     0.5f, -0.5f, 0.0f 
+		};
+		const GLint indices[INDEXSIZE] = { 
+			0, 1, 2,
+			1, 2, 3
+		};
+		GLuint VAO, VBO, EBO;
+		GLuint shaderProgram;
+		/*-----------------------------------*/
 		void getInput();
 		void renderContent();
+
 	public:
 		bool isRunning{ false };
 		bool setupWindow();
--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,4 @@
 #include "game.h"
-#include <glad/glad.h>
 
 bool Game::setupWindow() {
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -42,6 +41,39 @@ bool Game::setupWindow() {
 };
 
 void Game::gameLoop() {
+	glGenBuffers(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
+	/*-----Shader setup---------*/
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+	
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 	while(isRunning) {
 		getInput();
 		renderContent();
@@ -67,6 +99,12 @@ void Game::getInput() {
 void Game::renderContent() {	
 	glClearColor(0.769f, 0.51f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(shaderProgram);
+    glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        //glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);	
+		
+
 	SDL_GL_SwapWindow(m_window);
 }
 
